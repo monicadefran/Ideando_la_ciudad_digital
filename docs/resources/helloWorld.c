e9043cbc8640258bd62b7aedee00d6d45d30e990
Alvaro <ysinotelodigo@gmail.com>
2021-10-04 12:46:44 +0200

    Updating the projects

---
+++ docs/resources/helloWorld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    // Se mostrará Hola Mundo! en la pantalla
+    printf(“Hola Mundo!\n”);
+    return 0;
+}
+
+
+
+01010101001001001
+10101010101001010
+10101010101111100
+10010110100100000
+11010010010001010
+10010101001001110
